7111193ceed4b60c6eb563f85feff23276af28c8
Vic Mortelmans <vicmortelmans@gmail.com>
2022-05-17 23:09:13 +0200

    using gpiozero

--- helio.py
+++ helio.py
@@ -6,29 +6,24 @@
 
 # During development, it's invoked on command line
 
-# Define some constants:
-
-MOTOR_TRAVEL_TIME = 30  # seconds
-AMOTOR = 1
-HMOTOR = 2
-EXTEND = 1
-RETRACT = 2
-
 from consolemenu import *
 from consolemenu.items import *
-import motor
+from gpiozero import Motor
 import time
 
 # Setup
-MOTORH_FORWARD=26
-MOTORH_BACKWARD=20
-MOTORA_FORWARD=0
-MOTORA_BACKWARD=0
+MOTOR_TRAVEL_TIME = 30  # seconds
+MOTORH = Motor(26, 20)
+MOTORA = Motor(0, 0)
 
 
 def calibrate():
-    move_motor(MOTORH_BACKWARD, MOTOR_TRAVEL_TIME)
-    move_motor(MOTORA_BACKWARD, MOTOR_TRAVEL_TIME)
+    MOTORH.forward()
+    time.sleep(MOTOR_TRAVEL_TIME)
+    MOTORH.stop()
+    MOTORA.forward()
+    time.sleep(MOTOR_TRAVEL_TIME)
+    MOTORA.stop()
     # data =  [[]]
     # acs = []
     # xnrs = []
@@ -39,7 +34,9 @@ def calibrate():
         tcxss.append(tcxs)
         tcyss.append(tcys)
         tczss.append(tczs)
-        move_motor(HMOTOR, RETRACT, MOTOR_TRAVEL_TIME)
+        MOTORH.backward()
+        time.sleep(MOTOR_TRAVEL_TIME)
+        MOTORH.stop()
 
 
 def scan():
@@ -47,12 +44,12 @@ def scan():
     tcys = []
     tczs = []
     stop = time.time() + MOTOR_TRAVEL_TIME  # seconds
-    motor.start_moving_motor(HMOTOR, EXTEND)
+    MOTORH.forward()
     while time.time() < stop:
         tcxs.append(...)
         tcys.append(...)
         tczs.append(...)
-    motor.stop_moving_motor(HMOTOR)
+    MOTORH.stop()
     return tcxs, tcyx, tczs
 
 
