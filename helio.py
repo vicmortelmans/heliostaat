2012613711409a935c56b26c2230c7c2da610732
Vic Mortelmans <vicmortelmans@gmail.com>
2022-05-19 16:45:25 +0200

    random

--- helio.py
+++ helio.py
@@ -13,8 +13,11 @@ from consolemenu.items import *
 import csv
 from gpiozero import Motor
 import logging
+import numpy
+import random
 from scipy.signal import savgol_filter
 import time
+import vg
 
 # Setup logging
 logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d %(funcName)s] %(message)s', datefmt='%Y-%m-%d:%H:%M:%S', level=logging.DEBUG)
@@ -63,6 +66,14 @@ def calibrate():
         MOTORA.stop()
         remaining_amotor_travel_time -= partial_amotor_travel_time
         logging.info("Stop partially extend azimut motor")
+    logging.info("Start fully retract azimut motor")
+    MOTORA.backward()
+    time.sleep(MOTOR_TRAVEL_TIME)
+    logging.info("Stop fully retract azimut motor")
+    logging.info("Stop calibration")
+
+def save_calibration():
+    global cal
     logging.info("Writing data to file")
     with open('tcxss.csv', 'w') as f:
         writer = csv.writer(f)
@@ -82,16 +93,51 @@ def calibrate():
     with open('acxyzs.csv', 'w') as f:
         writer = csv.writer(f)
         writer.writerow(['x','y','z'])
-        for axyz in zip(cal['acxs'], cal['acys'], cal['aczs']):
-            writer.writerow(axyz)
-    logging.info("Start fully retract azimut motor")
-    MOTORA.backward()
-    time.sleep(MOTOR_TRAVEL_TIME)
-    logging.info("Stop fully retract azimut motor")
-    logging.info("Stop calibration")
+        for acxyz in zip(cal['acxs'], cal['acys'], cal['aczs']):
+            writer.writerow(acxyz)
+
 
 def load_calibration():
     global cal
+    with open('tcxss.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)
+        cal['tcxss'] = [list(tcxs) for tcxs in zip(*reader)]
+    with open('tcyss.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)
+        cal['tcyss'] = [list(tcys) for tcys in zip(*reader)]
+    with open('tczss.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)
+        cal['tczss'] = [list(tczs) for tcys in zip(*reader)]
+    with open('acxyzs.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)
+        cal['acxs'] = list(list(zip(*reader))[0])
+        cal['acys'] = list(list(zip(*reader))[1])
+        cal['aczs'] = list(list(zip(*reader))[2])
+
+
+def track_random():
+    global cal
+    acidx = random.randrange(0,len(cal['acxs']))
+    tcidx = random.randrange(0,len(cal['tcxss'][0]))
+    racx = cal['acxs'][acidx]
+    racy = cal['acys'][acidx]
+    racz = cal['aczs'][acidx]
+    rtcx = cal['tcxss'][acidx][tcidx]
+    rtcy = cal['tcyss'][acidx][tcidx]
+    rtcz = cal['tczss'][acidx][tcidx]
+    try:
+        while True:
+            tcx, tcy, tcz = sensor.magnetometer
+            acx, acy, acz = sensor.accelerometer
+            aangle = vg.angle(numpy.array([racx, racy, racz]), numpy.array([acx, acy, acz]))
+            tangle = vg.angle(numpy.array([rtcx, rtcy, rtcz]), numpy.array([tcx, tcy, tcz]))
+            print("azimut: {:4.1f} - heading: {:4.1f}\r".format(aangle, tangle))
+    except KeyboardInterrupt:
+        pass
 
 
 
@@ -118,7 +164,9 @@ def scan():
 def main():
     menu = ConsoleMenu("Heliostat", "Control Center")
     function_item = FunctionItem("Calibrate", calibrate)
+    function_item = FunctionItem("Save calibration to file", save_calibration)
     function_item = FunctionItem("Load calibration from file", load_calibration)
+    function_item = FunctionItem("Track angular distance to random calibration point", track_random)
     menu.append_item(function_item)
     menu.show()
 
